1dae22eb0fa9ee2c45020f08784216a28b46a047
MatinT-SA <matin.taherzadeh.mmtsa@gmail.com>
2025-02-21 16:34:32 +0330

    react-router-dom@6

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,34 @@
 import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./ui/Home";
+import Menu from "./features/menu/Menu";
+import Cart from "./features/cart/Cart";
+import Order from "./features/order/Order";
+import CreateOrder from "./features/order/CreateOrder";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/menu",
+    element: <Menu />,
+  },
+  {
+    path: "/cart",
+    element: <Cart />,
+  },
+  {
+    path: "/order/orderId",
+    element: <Order />,
+  },
+  {
+    path: "/order/new",
+    element: <CreateOrder />,
+  },
+]);
 
 export default function App() {
-  return <div>Zesty!</div>;
+  return <RouterProvider router={router} />;
 }
