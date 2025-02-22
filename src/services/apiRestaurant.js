be7ca8f5b8843010e191eb0382b6e3cda0c4cc8b
MatinT-SA <matin.taherzadeh.mmtsa@gmail.com>
2025-02-22 17:44:32 +0330

    data fetching from the api using react router dom library loader function

--- src/services/apiRestaurant.js
+++ src/services/apiRestaurant.js
@@ -1,13 +1,16 @@
 const API_URL = "https://react-fast-pizza-api.jonas.io/api";
 
 export async function getMenu() {
-  const res = await fetch(`${API_URL}/menu`);
+  try {
+    const res = await fetch(`${API_URL}/menu`);
 
-  // fetch won't throw error on 400 errors (e.g. when URL is wrong), so we need to do it manually. This will then go into the catch block, where the message is set
-  if (!res.ok) throw Error("Failed getting menu");
+    if (!res.ok) throw Error("Failed getting menu");
 
-  const { data } = await res.json();
-  return data;
+    const { data } = await res.json();
+    return data;
+  } catch (error) {
+    console.log(error.message);
+  }
 }
 
 export async function getOrder(id) {
