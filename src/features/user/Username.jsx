566a93c56b0c616685c0e485708f7ac478d80557
MatinT-SA <matin.taherzadeh.mmtsa@gmail.com>
2025-02-25 18:57:24 +0330

    flex tailwind

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Username() {
-  return <div className="text-sm font-semibold hidden">Matin</div>;
+  return <div className="text-sm font-semibold hidden md:block">Matin</div>;
 }
 
 export default Username;
