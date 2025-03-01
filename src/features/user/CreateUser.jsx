f5ff88458fc6d5d9c63241f81ea9f9081e74dc8f
MatinT-SA <matin.taherzadeh.mmtsa@gmail.com>
2025-03-01 15:50:31 +0330

    reading and updating the user state using rtk

--- src/features/user/CreateUser.jsx
+++ src/features/user/CreateUser.jsx
@@ -1,16 +1,25 @@
 import { useState } from "react";
 import Button from "../../ui/Button";
+import { useDispatch } from "react-redux";
+import { updateName } from "./userSlice";
+import { useNavigate } from "react-router-dom";
 
 function CreateUser() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   const [username, setUsername] = useState("");
 
   function handleSubmit(e) {
     e.preventDefault();
+
+    if (!username) return;
+    dispatch(updateName(username));
+    navigate("/menu");
   }
 
   return (
     <form onSubmit={handleSubmit}>
-      <p className="mb-4 text-sm md:text-base text-stone-600">
+      <p className="mb-4 text-sm text-stone-600 md:text-base">
         👋 Welcome! Please start by telling us your name:
       </p>
 
@@ -19,7 +28,7 @@ function CreateUser() {
         placeholder="Your full name"
         value={username}
         onChange={(e) => setUsername(e.target.value)}
-        className="w-72 input mb-8"
+        className="input mb-8 w-72"
       />
 
       {username !== "" && (
