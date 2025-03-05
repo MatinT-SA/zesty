d242c9fef1a2edf4d738a6fbe81e5f93d83b931b
MatinT-SA <matin.taherzadeh.mmtsa@gmail.com>
2025-03-05 14:12:50 +0330

    auto rotate on the z axis for the pizza

--- src/models/Pizza.jsx
+++ src/models/Pizza.jsx
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useRef } from "react";
 import { useGLTF } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
 
 function Pizza(props) {
   const { nodes, materials } = useGLTF("/3d/pizza.glb");
+  const pizzaRef = useRef();
+
+  useFrame(() => {
+    if (pizzaRef.current) {
+      pizzaRef.current.rotation.z += 0.004;
+    }
+  });
 
   return (
     <group {...props} dispose={null} scale={[13, 13, 13]}>
       <mesh
+        ref={pizzaRef}
         castShadow
         receiveShadow
         geometry={nodes.Object_2.geometry}
