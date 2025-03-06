fc015bd38898e84deeafc4f2d0a537646b3533a1
MatinT-SA <matin.taherzadeh.mmtsa@gmail.com>
2025-03-06 23:46:47 +0330

    adding logo next to name of restaurant

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -5,8 +5,16 @@ import Username from "../features/user/Username";
 
 function Header() {
   return (
-    <header className="bg-customOrange flex items-center justify-between border-b border-stone-200 px-4 py-3 uppercase sm:px-6">
-      <Link to="/" className="tracking-widest">
+    <header className="flex items-center justify-between border-b border-stone-200 bg-customOrange px-4 py-3 uppercase sm:px-6">
+      <Link
+        to="/"
+        className="flex items-center justify-center gap-2 tracking-widest"
+      >
+        <img
+          alt="logo"
+          src="/images/logo-tp.png"
+          className="lg:h-13 2xl:h-15 h-9 max-w-full sm:h-11 md:h-12 xl:h-14"
+        />{" "}
         Zesty Co.
       </Link>
 
