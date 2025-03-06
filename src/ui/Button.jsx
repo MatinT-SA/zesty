a1b9e72ec5bcccf862c52a4564bc1c5fc28644ae
MatinT-SA <matin.taherzadeh.mmtsa@gmail.com>
2025-03-06 23:24:54 +0330

    pressing down style for updating items buttons

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -8,7 +8,9 @@ function Button({ children, disabled, to, type, onClick }) {
   const styles = {
     primary: base + " px-4 py-3 sm:px-6 sm:py-4",
     small: base + " px-4 py-2 md:px-5 md:py-2.5 text-xs",
-    round: base + " px-2.5 py-1 md:px-3.5 md:py-2",
+    round:
+      base +
+      " px-2.5 py-1 md:px-3.5 md:py-2 active:scale-95 active:shadow-inner active:translate-y-0.5 active:shadow-lg active:shadow-gray-600 focus:ring-0 focus:ring-offset-0 transition-shadow duration-100",
     secondary:
       "inline-block text-sm rounded-full border-2 border-stone-300 px-4 py-3 font-semibold uppercase tracking-wide text-stone-400 transition-colors duration-300 hover:bg-stone-300 hover:text-stone-800 focus:bg-stone-300 focus:text-stone-800 focus:outline-none focus:ring focus:ring-stone-200 focus:ring-offset-2 disabled:cursor-not-allowed sm:px-6 sm:py-4 px-4 py-2.5 sm:px-6 sm:py-3.5",
   };
