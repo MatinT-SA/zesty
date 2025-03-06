bd01143aa76d09933515ca8ade967c962c4b6cba
MatinT-SA <matin.taherzadeh.mmtsa@gmail.com>
2025-03-06 23:39:43 +0330

    customizing empty cart page

--- src/features/cart/EmptyCart.jsx
+++ src/features/cart/EmptyCart.jsx
@@ -1,16 +1,31 @@
-import { ChevronsLeft } from "lucide-react";
+import { ChevronsLeft, Donut } from "lucide-react";
 import LinkButton from "../../ui/LinkButton";
+import PizzaScene from "../../ui/PizzaScene";
 
 function EmptyCart() {
   return (
-    <div className="px-4 py-3">
-      <LinkButton to="/menu">
-        <ChevronsLeft className="h-6 w-6 text-blue-500" /> Back to menu
+    <div className="rounded-lg px-6 py-6 text-center">
+      <LinkButton
+        to="/menu"
+        className="mb-4 flex items-center justify-center gap-2 transition-colors duration-300 hover:bg-blue-200 hover:text-blue-600"
+      >
+        <ChevronsLeft className="h-6 w-6 text-blue-500" /> Back to Menu
       </LinkButton>
 
-      <p className="mt-7 font-semibold">
-        Your cart is still empty. Start adding some pizzas :)
-      </p>
+      <div className="my-2">
+        <p className="mb-4 text-xl font-semibold text-stone-800">
+          Your cart is empty!
+        </p>
+        <p className="text-lg text-stone-600">
+          Looks like you've cleared your cart. No worries, you can always fill
+          it up with your favorite pizzas again!{" "}
+          <Donut className="inline-block h-6 w-6 text-customOrange" />
+        </p>
+      </div>
+
+      <div className="pizza mt-1 flex h-[18rem] items-center justify-center sm:h-[29rem]">
+        <PizzaScene />
+      </div>
     </div>
   );
 }
