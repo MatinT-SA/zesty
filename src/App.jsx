054e7630c66fe22d4a9c0a6bd0ef0ce1573c83d0
MatinT-SA <matin.taherzadeh.mmtsa@gmail.com>
2025-03-07 20:51:55 +0330

    adding basename attribute on RouterProvider

--- src/App.jsx
+++ src/App.jsx
@@ -48,5 +48,5 @@ const router = createBrowserRouter([
 ]);
 
 export default function App() {
-  return <RouterProvider router={router} />;
+  return <RouterProvider router={router} basename="/zesty" />;
 }
