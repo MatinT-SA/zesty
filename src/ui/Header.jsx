368f9d1efed98cc3dd9ec79eb9c220e9b5da8cb8
MatinT-SA <matin.taherzadeh.mmtsa@gmail.com>
2025-03-07 21:34:10 +0330

    fixing the url or out logo

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -12,7 +12,7 @@ function Header() {
       >
         <img
           alt="logo"
-          src={`${process.env.PUBLIC_URL}/images/logo-tp.png`}
+          src={`${process.env.PUBLIC_URL}/zesty/images/logo-tp.png`}
           className="lg:h-13 2xl:h-15 h-9 max-w-full sm:h-11 md:h-12 xl:h-14"
         />{" "}
         Zesty Co.
