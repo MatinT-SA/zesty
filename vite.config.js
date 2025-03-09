e96a453c7bee37946441b026def4385fc891266f
MatinT-SA <matin.taherzadeh.mmtsa@gmail.com>
2025-03-09 11:38:44 +0330

    gh-pages - 10

--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,8 @@ import eslint from "vite-plugin-eslint";
 
 export default defineConfig({
   plugins: [react(), eslint()],
-  base: process.env.NODE_ENV === "production" ? "/zesty/" : "/",
+  base: "/zesty/",
   server: {
-    host: true,
-    port: 5173,
-  },
-  build: {
-    outDir: "dist",
-  },
-  preview: {
-    port: 4173,
+    historyApiFallback: true,
   },
 });
